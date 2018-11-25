bfc208681a07ff30ce828b1678b14b77ddefb22b
Rohit Singh <c.ronnie.r@gmail.com>
2018-11-25 19:34:54 +0530

    Java Bean

---
+++ JSP/Calculator/packageCal/Calculator.java
@@ -0,0 +1,25 @@
+package packageCal;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Rohit
+ */
+public class Calculator implements Serializable
+{
+    private int x, y;
+    public Calculator(){}
+    
+    public int getX(){ return x; }
+    public void setX(int x){ this.x = x; }
+    
+    public int getY(){ return y; }
+    public void setY(int y){ this.y = y; }
+    
+    public int add(){ return x+y; }
+    public int minus(){ return x-y; }
+    public int multiply(){ return x*y; }
+    public float divide(){ return (float)(x/y); }
+    public int modulus(){ return x%y; }
+}
